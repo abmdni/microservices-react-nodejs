ff340b180a7dc415a60553c3d1e3a4dbd8fadaa5
AbdessamadNDay <abdessamad.yadini@gmail.com>
2022-01-02 00:07:09 +0100

    comment moderation service added

--- query/index.js
+++ query/index.js
@@ -20,12 +20,21 @@ app.post('/events', (req, res) => {
         posts[id] = { id, title, comments: [] };
     }
     if (type === 'CommentCreated') {
-        const { id, content, postId } = data;
+        const { id, content, postId, status } = data;
         const post = posts[postId];
-        console.log(post);
-        post.comments.push({ id, content });
+        post.comments.push({ id, content, status });
     }
-    console.log(posts);
+    if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data;
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        });
+        console.log(status);
+        comment.status = status;
+        comment.content = content;
+    }
+    console.log(type)
     res.send({});
 
 });
