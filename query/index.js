4ac7c9d18f57c99b90dc7f927abdf0252e6e5e9a
AbdessamadNDay <abdessamad.yadini@gmail.com>
2022-01-02 04:43:49 +0100

    sync services through string past events

--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
@@ -7,14 +8,8 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
+const handleEvent = (type, data) => {
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
     if (type === 'PostCreated') {
         const { id, title } = data;
         posts[id] = { id, title, comments: [] };
@@ -34,11 +29,25 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
-    console.log(type)
+};
+app.get('/posts', (req, res) => {
+    res.send(posts);
+
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    handleEvent(type, data);
     res.send({});
 
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on 4002, query service');
+    const res = await axios.get('http://localhost:4005/events');
+    for (let event of res.data) {
+        console.log('Processing event:', event.type);
+        handleEvent(event.type, event.data);
+    }
+
 });
